c2031a637077c2d2dee06155338a934bfa7f43e9
TheresaSchmotz <schmotz.theresa@gmail.com>
2019-04-09 09:28:02 +0200

    was geht da ned ich hab doch alles

--- js/user.js
+++ js/user.js
@@ -25,8 +25,24 @@ $(document).ready(function() {
         //5. clear entries from the form
         //6. maybe do something else... :-)
 
-        //your code follows here
+        //your code follows here:
+        //hommage an w3schools thx
 
+        var x = document.getElementById("username");
+        var newUsername = element.value;
+        userListBody.push(newUsername);
+        var list = userListBody.length;
+
+        var table = document.getElementById("table");
+        var row = table.insertRow(-1);
+
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+
+        cell1.innerHTML = list;
+        cell2.innerHTML = newUsername;
+        cell3.innerHTML = "<button type=\"button\" class=\"btn btn-secondary btn-danger deleteTrigger\" title=\"delete\"><i class=\"fa fa-trash\"></i></button>";
         return false;
     });
 
@@ -37,8 +53,8 @@ $(document).ready(function() {
         //2. update number of current users
 
         //your code follows here
-
-
+        var index = document.getElementById("index").value;
+        document.getElementById("table").deleteRow(index + 1);
     });
 
     //maybe some code follows here
